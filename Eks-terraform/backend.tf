3fc9e690cd0e31ba45b9516f603ed16a1db2b2f8
saravanadattareddy <127585712+saravanadattareddy@users.noreply.github.com>
2025-07-30 10:14:13 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "datta-123-1" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
